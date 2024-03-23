084d644e1736738d05cbaa6dc7de2fd1dfb6d42b
tomasz-umanski <tomasz.umanski@gmail.com>
2024-03-23 09:00:10 +0100

    feat: added basic views, controllers and styling

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'Hello World!';
+require_once 'src/controller/AppController.php';
+
+$controller = new AppController();
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+$action = explode("/", $path)[0];
+
+$controller->render($action);
